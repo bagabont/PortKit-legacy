9fd0a7bd787d8629f8f35c98fd5a5741d97ff092
Momchil Marinov <mmarinov@spbtv.com>
2015-01-13 17:04:30 +0100

    fixed bug in componentmodel

--- Source/Portkit.ComponentModel/ObservableObject.cs
+++ Source/Portkit.ComponentModel/ObservableObject.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.ComponentModel;
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
+﻿using System.ComponentModel;
 using System.Runtime.Serialization;
-using Portkit.Core.Extensions;
 
 namespace Portkit.ComponentModel
 {
@@ -29,40 +25,6 @@ namespace Portkit.ComponentModel
             {
                 handler(this, new PropertyChangedEventArgs(propertyName));
             }
-        }
-
-#if UNIVERSAL
-
-        /// <summary>
-        /// Sets a property value and fires the PropertyChanged event if the value is changed.
-        /// </summary>
-        /// <typeparam name="T">Property type.</typeparam>
-        /// <param name="storage">Backing field reference.</param>
-        /// <param name="value">New property value.</param>
-        /// <param name="propertyName">Property name.</param>
-        /// <returns>True if the property value is changed, false if the value instance is considered to be equal.</returns>
-        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (Equals(storage, value))
-            {
-                return false;
-            }
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-#endif
-
-        /// <summary>
-        /// Fires the PropertyChanged event.
-        /// </summary>
-        /// <typeparam name="T">Property type.</typeparam>
-        /// <param name="propertyExpression">Property expression, that provides the name of the property that changed,
-        ///  in order to avoid using "magic strings".</param>
-        protected virtual void SetProperty<T>(Expression<Func<T>> propertyExpression)
-        {
-            var propertyName = propertyExpression.GetPropertyName();
-            OnPropertyChanged(propertyName);
         }
     }
 }
--- Source/Portkit.ComponentModel/Presenter/PresenterObject.cs
+++ Source/Portkit.ComponentModel/Presenter/PresenterObject.cs
@@ -1,7 +1,10 @@
-﻿using Portkit.ComponentModel.Threading;
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Portkit.ComponentModel.Threading;
 using System;
 using System.ComponentModel;
 using System.Runtime.Serialization;
+using Portkit.Core.Extensions;
 
 namespace Portkit.ComponentModel.Presenter
 {
@@ -37,6 +40,37 @@ namespace Portkit.ComponentModel.Presenter
             {
                 base.OnPropertyChanged(propertyName);
             }
+        }
+
+        /// <summary>
+        /// Sets a property value and fires the PropertyChanged event if the value is changed.
+        /// </summary>
+        /// <typeparam name="T">Property type.</typeparam>
+        /// <param name="storage">Backing field reference.</param>
+        /// <param name="value">New property value.</param>
+        /// <param name="propertyName">Property name.</param>
+        /// <returns>True if the property value is changed, false if the value instance is considered to be equal.</returns>
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// Fires the PropertyChanged event.
+        /// </summary>
+        /// <typeparam name="T">Property type.</typeparam>
+        /// <param name="propertyExpression">Property expression, that provides the name of the property that changed,
+        ///  in order to avoid using "magic strings".</param>
+        protected virtual void SetProperty<T>(Expression<Func<T>> propertyExpression)
+        {
+            var propertyName = propertyExpression.GetPropertyName();
+            OnPropertyChanged(propertyName);
         }
     }
 }
